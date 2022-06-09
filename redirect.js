f75b2e4ab2dde76a28bd771729a453e78287b79e
Mexy combs <micheal.mahoney02@gmail.com>
2022-06-09 08:58:37 +0100

    another commit

---
+++ redirect.js
@@ -0,0 +1,17 @@
+
+
+  const base = 'https://a11-sd.pages.dev/';
+  const statusCode = 301;
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    const { pathname, search } = url;
+  
+    const destinationURL = base + pathname + search;
+  
+    return Response.redirect(destinationURL, statusCode);
+  }
+  
+  addEventListener('fetch', async event => {
+    event.respondWith(handleRequest(event.request));
+  });
